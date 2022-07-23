2422d821792fd63485f5c69631ecde3f5d2a878f
Ramprit Sahani <rampritsahani@outlook.in>
2022-07-23 11:44:33 +0530

    Adding database url

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 
+console.log(process.env.DATABASE_URL)
+
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      username: "postgres",
-      password: "plumtree",
-      port: 5433,
-      database: "authDB",
-      synchronize: true,
-      autoLoadEntities: true
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './apps/auth/.env', }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true, // This for development
+        autoLoadEntities: true,
+        url: config.get<string>("DATABASE_URL")
+      }),
+      inject: [ConfigService],
     }),
     UsersModule],
   controllers: [AuthController],
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger'
-import { createDocument } from './swagger/swagger';
 import { CrudConfigService } from '@nestjsx/crud';
+import * as helmet from 'helmet'
+import * as cookie from 'cookie-parser'
+import { createDocument } from './swagger/swagger';
 
 CrudConfigService.load({
   query: {
@@ -19,9 +21,21 @@ CrudConfigService.load({
 })
 
 import { AuthModule } from './auth.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create<NestExpressApplication>(AuthModule)
+  app.disable('x-powered-by')
+  app.setGlobalPrefix('api')
+  app.disable('x-powered-by')
+  app.enableCors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+  app.use(helmet.noSniff())
+  app.use(helmet.hidePoweredBy())
+  app.use(helmet.contentSecurityPolicy())
+  app.use(cookie())
+
   app.setGlobalPrefix('api')
   SwaggerModule.setup('api/docs', app, createDocument(app))
   await app.listen(3000);
